d732602e090fd42a68acf212850d5a65557132a3
nishibu555 <nishibu555@gmail.com>
2020-04-29 10:13:37 -0700

    origin changed

--- app/Http/Controllers/SuggestionController.php
+++ app/Http/Controllers/SuggestionController.php
@@ -4,11 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Suggestion;
+use App\Criterion;
 
 class SuggestionController extends Controller
 {
     public function getSuggestion(Request $request)
-    {
-       return $request;
+    {   
+        $c =array(); 
+        $r = [
+        	['name'=>'season', 'value'=>'autumn', 'operator'=>'='],
+        	['name'=>'season', 'value'=>'autumn', 'operator'=>'>']
+        ];
+    	foreach ($r as $criteria) {
+    		$shortName = Criterion::where('name', $criteria['name'])
+    		           ->Where('value', $criteria['value'])
+    		           ->where('operator', $criteria['operator'])
+    		           ->pluck('short_name');
+    		array_push($c, $shortName);           
+    	}
+    	dd($c);
+    }
+
+    public function updateWeight(Request $request)
+    {   
+    	Suggestion::where('category', $request->category)
+    	->where('criteria', $request->criteria)
+    	->update(['weight'=> $request->weight]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('/suggestion', 'SuggestionController@getSuggestion');
 Route::post('/suggestion', 'SuggestionController@setSuggestion');
+Route::post('/weight', 'SuggestionController@updateWeight');
